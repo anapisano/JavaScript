c0991e684fbf8d93e1e553e9b6591fac3ad7b292
Ana Pisano <ani_pisano@hotmail.com>
2022-07-04 21:29:56 -0300

    "desastre"

--- index.js
+++ index.js
@@ -38,23 +38,9 @@ console.log (sumar(10,15)) */
     return mipersonaje
 } */
 
-/* const personajes = [
-    {id: 1 , nombre: 'Ramoncito' , vida: 1000 },
-    {id: 2 , nombre: 'Eduardo' , vida: 1200 },
-    {id: 3 , nombre: 'Norma' , vida: 950 },
-    {id: 4 , nombre: 'Gerardo' , vida: 1150 },
-    {id: 5 , nombre: 'Francisca' , vida: 1400 },
-]
-
-
-for (mipersonsaje of personajes){
-    
-}
- */
-
 ///* entrega complementaria 2*///
 
-let cantidad = prompt ("¿Cuántas comidas favoritas tenés?")
+/* let cantidad = prompt ("¿Cuántas comidas favoritas tenés?")
 let comidasFavoritas = []
 
 for (i = 0; i < cantidad; i++){
@@ -78,3 +64,81 @@ if (lista == "si"){
 }else{
     alert ("Vuelva a intentarlo, opción incorrecta")
 }
+ */
+
+
+
+
+
+const listaProductos = [
+    {
+        id: 1 , 
+        nombre: `Piedras de equilibrio` ,
+        categoria: `Madera`,
+        precio: 800,
+        stock: 4,
+    },
+    {
+        id: 2 , 
+        nombre: `Casita de tela` ,
+        categoria: `Tela`,
+        precio: 2600,
+        stock: 5,
+    },
+    {
+        id: 3 , 
+        nombre: `Animales Puna` ,
+        categoria: `Madera`,
+        precio: 1700,
+        stock: 8,
+    },
+    {
+        id: 4 , 
+        nombre: `Corona` ,
+        categoria: `Tela`,
+        precio: 650,
+        stock: 7,
+    },
+]
+
+
+const listasNombres = listaProductos.map ((producto) => producto.nombre)
+const listaPrecios = listaProductos.map ((producto) => producto.precio)
+
+alert("¡BIENVENIDXS A A TIENDA VIRTUAL DE CRÍA SUELTA!")
+
+let siNo = prompt ("A continuacion le detallamos nuestra lista de productos: \n\n"+ listasNombres + "\n\n¿desea conocer el precio de alguno de nuestros productos?\n\n1.Si\n2.No, gracias" )
+
+if (siNo == 1){
+    let eleccion = prompt (`¿De cuál?\n\n1. ` + listasNombres[0] + `\n2. ` + listasNombres[1] + `\n3. ` + listasNombres[2] + `\n4. `+ listasNombres[3] + `\n5. De todos los productos`)
+
+        if (eleccion == 1){
+            let carrito = prompt (`$` + listaPrecios[0] + `\n¿Agregar al carrito?\n\n1.Si\n2.No`)
+
+            if (carrito == 1 ){  
+                alert (listasNombres[0] + ` agregado correctamente`)
+            }else{
+                alert ("Gracias por visitarnos =)")
+            }
+        }else if (eleccion == 2){
+            prompt (`$` + listaPrecios[1] + `\n¿Agregar al carrito?`)
+        }else if (eleccion == 3){
+            prompt (`$` + listaPrecios[2] + `\n¿Agregar al carrito?`)
+        }else if (eleccion == 4){
+            prompt (`$` + listaPrecios[3] + `\n¿Agregar al carrito?`)
+        }else if (eleccion == 5){
+            prompt (listasNombres[0] + ` $` + listaPrecios[0] + `\n`+ listasNombres[1] + ` $` + listaPrecios[1] + `\n` + listasNombres[2] + ` $` + listaPrecios[2] + `\n` + listasNombres[3] + ` $` + listaPrecios[3])
+        }else{
+            alert ("Opción inválida")
+        }
+
+}else if (siNo == 2){
+    alert ("Gracias por visitarnos =)")
+
+}else{
+    alert ("Opción inválida")
+}
+
+prompt ("¿Desea realizar alguna compra?")
+
+
